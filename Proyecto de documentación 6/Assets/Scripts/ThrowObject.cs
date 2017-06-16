ee22821d9198faf1e53a1f59865b302359db97e1
RubenCr <RubenCr@users.noreply.github.com>
2017-06-15 18:56:02 -0600

    ThrowObject Updated
    Now, you take only the apples in the middle of the screen.

--- Proyecto de documentación 6/Assets/Scripts/ThrowObject.cs
+++ Proyecto de documentación 6/Assets/Scripts/ThrowObject.cs
@@ -5,6 +5,7 @@ public class ThrowObject : MonoBehaviour
 {
     private Transform player;
     private Transform playerCam;
+    public float distanceToBeCarried;
     public float throwForce = 10;
     bool hasPlayer = false;
     bool beingCarried = false;
@@ -12,6 +13,7 @@ public class ThrowObject : MonoBehaviour
     private AudioSource audio;
     public int dmg;
     private bool touched = false;
+    private Camera camr;
 
     void Start()
     {
@@ -24,7 +26,8 @@ public class ThrowObject : MonoBehaviour
         playerCam = GameObject.Find("FirstPersonCharacter").GetComponent<Transform>();
 
         float dist = Vector3.Distance(gameObject.transform.position, player.position);
-        if (dist <= 4.0f)
+
+        if (dist <= distanceToBeCarried)
         {
             hasPlayer = true;
         }
@@ -34,36 +37,57 @@ public class ThrowObject : MonoBehaviour
         }
         if (beingCarried)
         {
-            if (touched)
-            {
-                GetComponent<Rigidbody>().isKinematic = false;
-                transform.parent = null;
-                beingCarried = false;
-                touched = false;
-            }
-            if (Input.GetMouseButtonDown(0))
-            {
-
-                GetComponent<Rigidbody>().isKinematic = false;
-                transform.parent = null;
-                beingCarried = false;
-                GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce);
-                //RandomAudio();
-            }
-            else if (Input.GetMouseButtonDown(1))
-            {
-                GetComponent<Rigidbody>().isKinematic = false;
-                transform.parent = null;
-                beingCarried = false;
-            }
-        } else 
-        if (hasPlayer && Input.GetMouseButtonDown(0) && !beingCarried)
-        {
-            GetComponent<Rigidbody>().isKinematic = true;
-            transform.parent = playerCam;
-            beingCarried = true;
+            throwAppleIfNecessary();
+        }
+        else {
+            takeAppleIfPossible();
         }
     }
+
+    void takeAppleIfPossible()
+    {
+        RaycastHit hit;
+        camr = playerCam.GetComponentInParent<Camera>();
+        Vector3 rayOrigin = camr.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+        Debug.DrawRay(rayOrigin, camr.transform.forward * (distanceToBeCarried + 1));
+
+        if (Physics.Raycast(rayOrigin, camr.transform.forward, out hit, (distanceToBeCarried + 1)))
+        {
+            if (hasPlayer && Input.GetMouseButtonDown(0) && !beingCarried && 
+                hit.collider.name == gameObject.name)
+            {
+                GetComponent<Rigidbody>().isKinematic = true;
+                transform.parent = playerCam;
+                beingCarried = true;
+            }
+        }
+    }
+
+    void throwAppleIfNecessary()
+    {
+        if (touched)
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+            transform.parent = null;
+            beingCarried = false;
+            touched = false;
+        }else
+        if (Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+            transform.parent = null;
+            beingCarried = false;
+            GetComponent<Rigidbody>().AddForce(playerCam.forward * throwForce);
+            //RandomAudio();
+        }
+        else if (Input.GetMouseButtonDown(1))
+        {
+            GetComponent<Rigidbody>().isKinematic = false;
+            transform.parent = null;
+            beingCarried = false;
+        }
+    }
+
     void RandomAudio()
     {
         if (audio.isPlaying)
